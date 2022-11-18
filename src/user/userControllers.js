be121d6231bfcd86ff367e9d7c31f94ea6a1b6f7
RowDog <mrdiamonddirt@gmail.com>
2022-11-18 16:39:40 +0000

    added all crud functions and checked

--- src/user/userControllers.js
+++ src/user/userControllers.js
@@ -21,3 +21,28 @@ exports.listUsers = async (request, response) => {
     response.status(500).send({ error: error.message });
   }
 }
+
+exports.updateUser = async (request, response) => {
+  console.log(request);
+  try {
+  //  get user by username and update password
+  console.log('trying to update user');
+  const user  = await User.findOneAndUpdate({ users: request.body.username }, { password: request.body.password }, );
+  console.log('user updated');
+  response.status(200).send({ user });
+  } catch (error) {
+    console.log(error);
+    response.status(500).send({ error: error.message });
+  }
+}
+
+exports.deleteUser = async (request, response) => {
+  console.log(request);
+  try {
+    const user = await User.findOneAndDelete({ users: request.body.username }, { password: request.body.password });
+    response.status(200).send({ user });
+  } catch (error) {
+    console.log(error);
+    response.status(500).send({ error: error.message });
+  }
+}
--- src/user/userRouter.js
+++ src/user/userRouter.js
@@ -1,9 +1,12 @@
 const {Router} = require('express');
-const { createUser, listUsers } = require('./userControllers.js');
+const { createUser, listUsers, updateUser, deleteUser } = require('./userControllers.js');
 const userRouter = Router();
 
 // userRouter.get('/listUsers', listUsers);
 userRouter.post('/addUser', createUser);
 userRouter.get('/listUsers', listUsers);
+userRouter.put('/updateUser', updateUser);
+userRouter.delete('/deleteUser', deleteUser);
+
 
 module.exports = userRouter;
