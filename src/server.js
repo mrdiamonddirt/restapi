a405e5aef6bc1bba2e9be2e9647da2aea550d4fe
RowDog <mrdiamonddirt@gmail.com>
2022-12-12 11:54:26 +0000

    added web socket connection

--- src/server.js
+++ src/server.js
@@ -1,16 +1,30 @@
-require('./db/connection.js');
+require("./db/connection.js");
 
-const express = require('express');
-const userRouter = require('./user/userRouter.js');
+const express = require("express");
+const userRouter = require("./user/userRouter.js");
 
 const app = express();
-const port = process.env.Port || 5001;
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server);
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
+});
+
+server.listen(3000, () => {
+    console.log("server started");
+});
+
+const port = process.env.Port || 5001;
 
 app.use(express.json());
 app.use(userRouter);
 
-
-app.listen(port , () => {
-    console.log(`Server is running on port ${port}`)
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
