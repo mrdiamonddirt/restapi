8d20258111607c3f0d7d39e5becb25b5c373ef9f
RowDog <mrdiamonddirt@gmail.com>
2022-12-12 12:29:36 +0000

    added web socker detect when user connect and disconnect

--- src/server.js
+++ src/server.js
@@ -1,16 +1,35 @@
-require('./db/connection.js');
+require("./db/connection.js");
 
-const express = require('express');
-const userRouter = require('./user/userRouter.js');
+const express = require("express");
+const userRouter = require("./user/userRouter.js");
 
 const app = express();
-const port = process.env.Port || 5001;
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
+const port = process.env.Port || 5001;
+const socketPort = 5002;
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+    });
+});
+
+server.listen(socketPort, () => {
+    console.log(`Socket.IO server running at http://localhost:${socketPort}/`);
+});
 
 app.use(express.json());
 app.use(userRouter);
 
-
-app.listen(port , () => {
-    console.log(`Server is running on port ${port}`)
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
