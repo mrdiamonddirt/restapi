35c75db5d019feeca567c91271c8c9df8cd916fc
RowDog <mrdiamonddirt@gmail.com>
2022-12-12 12:33:36 +0000

    log messages sent by client on server

--- src/server.js
+++ src/server.js
@@ -17,6 +17,9 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
+    socket.on("chat message", (msg) => {
+        console.log("message: " + msg);
+    });
     console.log("a user connected");
     socket.on("disconnect", () => {
         console.log("user disconnected");
