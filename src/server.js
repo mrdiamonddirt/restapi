17e5d306797e0d5aa624e55075600b4a998c90c1
RowDog <mrdiamonddirt@gmail.com>
2022-12-12 17:03:41 +0000

    add check for amount of users connected

--- src/server.js
+++ src/server.js
@@ -16,10 +16,44 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
+// a variable for all the connected users
+let users = [];
+// on connect event add user to users array
+
 io.on("connection", (socket) => {
+    // add user to users array
+    users[socket.id] = {
+        socket_id: socket.id,
+    };
+    // send message to client when connected
+    socket.send("Welcome to the chat");
+    // listen for messages from client
     socket.on("chat message", (msg) => {
         console.log("message: " + msg);
         io.emit("chat message", msg);
+        // if message is hi, send hello back
+        if (msg === "hi") {
+            io.emit("chat message", "hello");
+        }
+        // if message is get users, send users back
+        if (msg === "get users") {
+            // socket.send(
+            //     "users: " + users.map((user) => user.socket_id).join(", ")
+            // );
+            io.emit(
+                "chat message",
+                Object.keys(users).length + " users connected"
+            );
+            console.log(users);
+            // console log an objects keys and values to a table
+            console.table(
+                Object.keys(users).map((key) => {
+                    return users[key];
+                })
+            );
+            // console log the number of keys in an object
+            console.log(Object.keys(users).length);
+        }
     });
     console.log("a user connected");
     socket.on("disconnect", () => {
